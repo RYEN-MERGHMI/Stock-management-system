23894ab8d82544694b2f680094aef96cc7c323b5
RAYEN-MERGHMI <merghmirayen@gmail.com>
2024-09-10 09:46:02 +0100

    start creating validators

---
+++ Stock-Managment-System/src/main/java/dev/rayen/Stock/validator/CategoryValidator.java
@@ -0,0 +1,11 @@
+/**
+ * Made  by rayen.
+ * Date: 10/09/2024.
+ * Time: 09:37.
+ * Project Name : Stock-Managment-System.
+ */
+
+package dev.rayen.Stock.validator;
+
+public class CategoryValidato
+}
