87cbf8c25ddc04ae9017299ff8a2494068ce2c71
RAYEN-MERGHMI <merghmirayen@gmail.com>
2024-09-10 11:04:59 +0100

    complete the Entity not found exception

--- Stock-Managment-System/src/main/java/dev/rayen/Stock/exeption/EntityNotFoundException.java
+++ Stock-Managment-System/src/main/java/dev/rayen/Stock/exeption/EntityNotFoundException.java
@@ -8,4 +8,23 @@
 package dev.rayen.Stock.exeption;
 
 public class EntityNotFoundException extends  RuntimeException {
+
+    private ErrorCodes errorCodes ;
+
+    public EntityNotFoundException(String message){
+        super(message);
+    }
+
+    public EntityNotFoundException(String message , Throwable cause){
+        super(message , cause);
+    }
+     public EntityNotFoundException(String mes , Throwable cause , ErrorCodes errorCodes){
+        super(mes , cause);
+        this.errorCodes = errorCodes ;
+     }
+
+     public EntityNotFoundException(String message ,  ErrorCodes error){
+        super(message);
+        this.errorCodes = error ;
+     }
 }
