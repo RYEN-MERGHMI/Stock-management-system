0c024eec7b4c9c6e01b53def7f52b45091443264
RAYEN-MERGHMI <merghmirayen@gmail.com>
2024-09-10 11:00:25 +0100

    start creating the excepiton classes

---
+++ Stock-Managment-System/src/main/java/dev/rayen/Stock/exeption/EntityNotFoundException.java
@@ -0,0 +1,11 @@
+/**
+ * Made  by rayen.
+ * Date: 10/09/2024.
+ * Time: 10:58.
+ * Project Name : Stock-Managment-System.
+ */
+
+package dev.rayen.Stock.exeption;
+
+public class EntityNotFoundException extends  RuntimeException {
+}
---
+++ Stock-Managment-System/src/main/java/dev/rayen/Stock/exeption/ErrorCodes.java
@@ -0,0 +1,50 @@
+package dev.rayen.Stock.exeption;
+
+public enum ErrorCodes {
+
+    ARTICLE_NOT_FOUND(1000),
+    ARTICLE_NOT_VALID(1001),
+    ARTICLE_ALREADY_IN_USE(1002),
+
+    CATEGORY_NOT_FOUND(2000),
+    CATEGORY_NOT_VALID(2001),
+    CATEGORY_ALREADY_IN_USE(2002),
+
+
+    CLIENT_NOT_FOUND(3000),
+    CLIENT_NOT_VALID(3001),
+    CLIENT_ALREADY_IN_USE(3002),
+
+
+    ORDER_CLIENT_NOT_FOUND(4000),
+
+
+    ORDER_SUPPLIER_NOT_FOUND(5000),
+
+    COMPANY_NOT_FOUND(6000),
+
+    SUPPLER_NOT_FOUND(7000),
+    CLIENT_CODE_LINE_NOT_FOUND(8000),
+    SUPPLIER_CODE_LINE_NOT_FOUND(9000),
+    SALE_LINE_NOT_FOUND(1000),
+    STCK_MVMNT_NOT_FOUND(11000),
+
+    USERS_NOT_FOUND(12000),
+
+
+    BAD_CREDENTIALS(12003),
+
+    SALE_NOT_FOUND(13000);
+
+
+
+    private int code;
+
+    ErrorCodes(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
